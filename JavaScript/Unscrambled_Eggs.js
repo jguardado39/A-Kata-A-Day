ba44e66ed4b37d1692b199fe415efcacad131885
jguardado39 <jguardado39@gmail.com>
2017-07-11 19:39:11 -0700

    done

---
+++ JavaScript/Unscrambled_Eggs.js
@@ -0,0 +1,21 @@
+// Modified: 07/11/2017
+//
+// Author: jguardado39 (John Guardado)
+
+/*
+https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7
+
+Unscramble the eggs.
+
+The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+
+example:
+
+unscrambleEggs("Beggegeggineggneggeregg"); => Beginner
+
+Kata is supposed to be for beginners to practice regular expressions, so commenting would be appreciated.
+*/
+
+function unscrambleEggs(word){
+  return word.replace(/egg/g,"");
+}
--- README.md
+++ README.md
@@ -11,6 +11,6 @@ I have taken up the challenge to solve 1 Kata everyday from CodeWars. I will be 
 |----------|:-------------:|
 | [JAVA](https://github.com/jguardado39/A-Kata-A-Day/tree/master/Java) | 2 | 
 | [Python](https://github.com/jguardado39/A-Kata-A-Day/tree/master/Python) | 54 | 
-| [JavaScript](https://github.com/jguardado39/A-Kata-A-Day/tree/master/JavaScript) | 3 | 
+| [JavaScript](https://github.com/jguardado39/A-Kata-A-Day/tree/master/JavaScript) | 4 | 
 
 My CodeWars profile: http://www.codewars.com/users/jguardado39
