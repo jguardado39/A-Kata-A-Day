b3aa2daca463027fa2faa773067f2970fe802df0
jguardado39 <jguardado39@gmail.com>
2017-07-11 18:25:38 -0700

    done

---
+++ Java/Multiply.java
@@ -0,0 +1,16 @@
+// Modified: 04/06/2017
+//
+// Author: jguardado39 (John Guardado)
+
+
+/**
+*  https://www.codewars.com/kata/50654ddff44f800200000004
+*
+*  The code does not execute properly. Try to figure out why.
+*/
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
