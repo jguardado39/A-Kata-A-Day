e04d7361b48099d6225bf130ba97ea4cdd4c8a83
jguardado39 <jguardado39@gmail.com>
2017-07-11 18:22:26 -0700

    done

---
+++ Python/Multiply.py
@@ -0,0 +1,13 @@
+# Modified: 07/11/2017
+#
+# Author: jguardado39 (John Guardado)
+
+"""
+https://www.codewars.com/kata/50654ddff44f800200000004
+
+The code does not execute properly. Try to figure out why.
+
+"""
+
+def multiply(a, b):
+  return a * b
