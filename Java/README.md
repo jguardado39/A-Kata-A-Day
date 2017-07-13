761c643919602e47392ef2fdba5662673e631988
jguardado39 <jguardado39@gmail.com>
2017-07-12 20:35:02 -0700

    Updated Count

--- Java/README.md
+++ Java/README.md
@@ -4,4 +4,5 @@
 2. Find Divisors Number 
 3. Find the First Non-Consercutive Number
 4. Multiply
+5. Triangular Treasure
 
---
+++ Java/Triangular_Treasure.java
@@ -0,0 +1,42 @@
+// Modified: 07/12/2017
+//
+// Author: jguardado39 (John Guardado)
+
+/*
+
+https://www.codewars.com/kata/525e5a1cb735154b320002c8
+
+Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+1st (1)   2nd (3)    3rd (6)
+*          **        ***
+           *         **
+                     *
+
+You need to return the nth triangular number. You should return 0 for out of range values:
+
+all [
+  triangular 0     == 0,
+  triangular 2     == 3,
+  triangular 3     == 6,
+  triangular (-10) == 0
+] -- True
+
+
+*/
+
+public class Triangular {
+    public static int triangular(int n) {
+    	if (n < 0) {
+      	return 0;
+      }
+    return n * (n + 1) / 2;
+    }
+}
+
+// By JulianNicholls
+public class Triangular_alt {
+    public static int triangular(int n) {
+        return (n <= 0) ? 0 : n * (n + 1) / 2;
+    }
+}
